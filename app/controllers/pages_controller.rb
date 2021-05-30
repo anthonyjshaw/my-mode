b34171e0a1725f028fd2f05aa4a2ae65b780cb99
Anthony Shaw <anthonyshaw@sceaga.com>
2021-05-30 12:22:49 +0100

    added pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: %i[home men women search]
 
   def home
+  end
+
+  def men
+  end
+
+  def women
+  end
+
+  def search
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get '/men', to: 'pages#men' do
+  get '/search', to: 'pages#search'
+  get '/men', to: 'pages#men', as: :men do
 
   end
-  get '/women', to: 'pages#women' do
+  get '/women', to: 'pages#women', as: :women do
 
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
