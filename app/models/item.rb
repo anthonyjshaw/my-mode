f4e6e434eca2fb89566bfd4d3cbfe8b5fae29153
Anthony Shaw <anthonyshaw@sceaga.com>
2021-05-30 13:17:25 +0100

    Added numericality validation to item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,12 @@
 class Item < ApplicationRecord
   validates_presence_of :clothes_type,
-  :color,
-  :description,
-  :size,
-  :price,
-  :quantity,
-  :name,
-  :is_in_stock,
-  :gender
+                        :color,
+                        :description,
+                        :size,
+                        :price,
+                        :quantity,
+                        :name,
+                        :is_in_stock,
+                        :gender
+  validates_numericality_of :price, { greater_than: 0.0 }, :quantity
 end
