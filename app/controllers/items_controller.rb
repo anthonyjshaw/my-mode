dc0c2f20cd676d8fe39e6565d7132f5cdb73cdba
Anthony Shaw <anthonyshaw@sceaga.com>
2021-05-30 15:11:03 +0100

    Edited parameters for items  controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :clothes_type, :size, :)
+    params.require(:item).permit(:name, :clothes_type, :size, :quantity, :price, :)
   end
 end
