f25bc739889741d00eb621c4e4e2fc980b020c07
Anthony Shaw <anthonyshaw@sceaga.com>
2021-06-18 12:59:24 +0100

    Added name and user reference to style model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_073430) do
+ActiveRecord::Schema.define(version: 2021_06_18_115840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,9 @@ ActiveRecord::Schema.define(version: 2021_06_17_073430) do
   create_table "styles", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_styles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -68,4 +71,5 @@ ActiveRecord::Schema.define(version: 2021_06_17_073430) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "styles", "users"
 end
