136c6ba2e734ee6bc42bfbbcffc3191069e63a00
Anthony Shaw <anthonyshaw@sceaga.com>
2021-06-24 17:12:59 +0100

    Reconfigured test

--- test/models/style_test.rb
+++ test/models/style_test.rb
@@ -12,11 +12,4 @@ class StyleTest < ActiveSupport::TestCase
     assert_not style.save, 'User is not present'
   end
 
-  # test 'should be able to save styles' do
-  #   user1 = User.create(email: 'ant@ant.com', username: 'username', password: '123456')
-  #   user2 = User.create(email: 'dave@ant.com', username: 'dave', password: '123456')
-  #   style = Style.create(user: user1, name: "Summer look", description: 'Looking to test a new summer look')
-  #   user2.favorite(style).valid?
-  # end
-
 end
