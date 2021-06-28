4339cdf98172465ee0d38369f8e00b757892b74b
Anthony Shaw <anthonyshaw@sceaga.com>
2021-06-29 00:02:56 +0100

    Added eslint

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require('@rails/ujs').start()
+require('turbolinks').start()
+require('@rails/activestorage').start()
+require('channels')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -44,7 +44,6 @@ document.addEventListener('turbolinks:load', () => {
   initServiceWorker();
   addDisableAttrToItemSelect();
   disableCommentBtn();
-
 
 });
 
--- app/javascript/packs/disableCommentBtn.js
+++ app/javascript/packs/disableCommentBtn.js
@@ -3,14 +3,13 @@ const disableCommentBtn = () => {
   const commentContent = document.getElementById('comment_content');
   const commentSubmitBtn = document.getElementById('comment-submit-button')
   if (commentContent) {
-
-  commentContent.addEventListener('keyup', (event) =>{
-  if (commentContent.value !== '') {
-    commentSubmitBtn.disabled = false;
-  } else {
-    commentSubmitBtn.setAttribute('disabled', '') ;
-  }
-  });
+    commentContent.addEventListener('keyup', (event) => {
+      if (commentContent.value === '') {
+        commentSubmitBtn.setAttribute('disabled', '');
+      } else {
+        commentSubmitBtn.disabled = false;
+      }
+    });
   }
 
 }
