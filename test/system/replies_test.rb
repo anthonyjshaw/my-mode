7adbfc8f9bd92384bcdef58f717122f6ff52d733
Anthony Shaw <anthonyshaw@sceaga.com>
2021-06-29 23:05:02 +0100

    Reconfigured replies system test

--- test/system/replies_test.rb
+++ test/system/replies_test.rb
@@ -4,6 +4,7 @@ class RepliesTest < ApplicationSystemTestCase
 
   test 'replies should be present under comment' do
     visit style_url(Style.find_by(name: 'Summer Look'))
+    click_on 'Show replies'
     assert_selector 'p', text: 'Thanks!'
   end
 
