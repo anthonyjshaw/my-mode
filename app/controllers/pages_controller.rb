c3c547a7ac071c91bbded061e5358d9faf6453a9
Anthony Shaw <anthonyshaw@sceaga.com>
2021-06-29 22:59:34 +0100

    Added styling to show and about pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
 
   def home
     @feed = policy_scope(Style)
-      .includes([:user, [photo_attachment: :blob]]).order(created_at: :desc).first(3)
+      .includes([:user, [photo_attachment: :blob]]).where.not(user: current_user).order(created_at: :desc).first(3)
     @my_styles = policy_scope(Style)
       .includes([:user, [photo_attachment: :blob]]).where(user: current_user).order(created_at: :desc).first(3)
     @liked_styles = user_signed_in? ? current_user.favorited_by_type('Style').includes([:user]).order(created_at: :asc).first(3) : []
